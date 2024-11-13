fe40c09e918bab1797582efd5fbc43b17d1dca9d
awais144506 <awaisrasheed68@gmail.com>
2024-11-14 00:26:37 +0500

    Done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -55,14 +55,25 @@ router.post("/", (req, res) => {
 
 
 router.put("/:id", (req, res) => {
-    const { id } = req.params
-    if (!id) {
-        res.status(404).json({ message: "The post with the specified ID does not exist" })
+    const { title, contents } = req.body
+    if (!title || !contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" })
     }
     else {
-        const { title, contents } = req.body
-        if (!title || !contents) res.status(400).json("Please provide title and contents for the post")
-        Post.update(id, { title, contents })
+        Post.findById(req.params.id)
+            .then(stuff => {
+                if (!stuff) {
+                    res.status(404).json({ message: "The post with the specified ID does not exist" })
+                }
+                else {
+                    return Post.update(req.params.id, req.body)
+                }
+            })
+            .then(data => {
+                if (data) {
+                    return Post.findById(req.params.id)
+                }
+            })
             .then(post => {
                 res.status(200).json(post)
             })
@@ -94,7 +105,21 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
-router.get('/:id/comments',(req,res)=>{
-    
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }
+        else {
+            const stuff = await Post.findPostComments(req.params.id)
+            res.json(stuff)
+        }
+    }
+    catch (err) {
+        res.status(500).json({
+            message: "The posts information could not be retrieved"
+        })
+    }
 })
 module.exports = router
